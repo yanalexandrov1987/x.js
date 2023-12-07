a549f267c57ab9cf127fb61b56c8336bd76d69af
Yan <yanalexandrov@gmail.com>
2023-12-07 18:22:35 +0300

    Add $fetch method

--- src/scripts/methods/$fetch.js
+++ src/scripts/methods/$fetch.js
@@ -1,30 +1,73 @@
 import { method } from '../methods';
 
-method('fetch', (e, el) => url => {
-  let formData = new FormData();
+method('fetch', (e, el) => (url, options = {}, callback) => {
+  let tagName = el.tagName.toLowerCase(),
+      method  = tagName === 'form' ? 'post' : 'get',
+      data    = tagName === 'form' ? new FormData(el) : new FormData(),
+      xhr     = new XMLHttpRequest();
 
-  return new Promise(resolve => {
-    let method  = el.getAttribute('method')?.toUpperCase() ?? 'GET',
-        request = new XMLHttpRequest();
-
-    request.withCredentials = true;
-    request.responseType    = 'json';
-
-    request.open(method, url);
-    request.send(formData);
-
-    request.onload = () => {
-
-    }
-
-    request.upload.onprogress = event => {
-      console.log(`Progress ${parseInt(event.loaded / event.total * 100)}%`);
-    }
-
-    request.onreadystatechange = () => {
-      if (request.response) {
-        console.log(request.response)
+  // fill formData in accordance with the type of fields
+  switch (tagName) {
+    case 'form':
+      Array.from(el.querySelectorAll("input[type='file']")).forEach(input => {
+        input.files && [...input.files].forEach(file => data.append(input.name, file));
+      });
+      break;
+    case 'textarea':
+    case 'select':
+    case 'input':
+      if (el.type === 'file' && el.files) {
+        Array.from(el.files).forEach(file => data.append(el.name, file));
+      } else {
+        el.name && data.append(el.name, el.value);
       }
-    };
+      break;
+  }
+
+  return new Promise((resolve, reject) => {
+    xhr.open(method, url);
+
+    for (const i in options.headers) {
+      xhr.setRequestHeader(i, options.headers[i]);
+    }
+
+    xhr.onerror         = reject;
+    xhr.withCredentials = options.credentials === 'include';
+
+    // send ajax request with file uploading
+    xhr.upload.onprogress = event => callback && callback(onProgress(event));
+
+    // regular ajax sending
+    xhr.onloadstart = event => callback && callback(onProgress(event));
+    xhr.onload      = event => resolve(() => onProgress(xhr));
+    xhr.onloadend   = event => callback && callback(onProgress(event));
+
+    xhr.send(data);
+  }).then(response => {
+    return response();
   });
 });
+
+function onProgress(data) {
+  const BYTES_IN_MB = 1048576;
+  if (data.loaded) {
+    return {
+      start: true,
+      progress: false,
+      end: false,
+      loaded: (data.loaded/BYTES_IN_MB).toFixed(1),
+      total: (data.total/BYTES_IN_MB).toFixed(1),
+      percent: 0,
+    }
+  }
+
+  return {
+    blob: new Blob([data.response]),
+    json: JSON.parse(data.responseText),
+    response: data.response,
+    status: data.status,
+    statusText: data.statusText,
+    text: data.responseText,
+    url: data.responseURL,
+  }
+}
