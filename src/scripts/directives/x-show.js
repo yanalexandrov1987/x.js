3b50f380644f96223bc4cd3378979f0e3344fbff
Yan <yanalexandrov@gmail.com>
2023-12-17 21:27:10 +0300

    Fix bugs

--- src/scripts/directives/x-show.js
+++ src/scripts/directives/x-show.js
@@ -1,5 +1,6 @@
 import { directive } from '../directives';
 
 directive('show', (el, expression, attribute, x, component) => {
+  console.log(attribute)
   el.style.display = expression ? 'block' : 'none'
 });
