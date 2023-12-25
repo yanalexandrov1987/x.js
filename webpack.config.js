2f1602b31fb9f7e459b96e74acd0116941a4ffd3
Yan <yanalexandrov@gmail.com>
2023-12-25 19:47:03 +0300

    Add support kebabcase for props

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,15 @@
 const fs   = require('fs');
 const path = require('path');
+const glob = require('glob');
 
 const CopyPlugin             = require('copy-webpack-plugin');
 const TerserPlugin           = require('terser-webpack-plugin');
-const HtmlWebpackPlugin      = require('html-webpack-plugin');
 const CssMinimizerPlugin     = require('css-minimizer-webpack-plugin');
+const HtmlWebpackPlugin      = require('html-webpack-plugin');
 const MiniCssExtractPlugin   = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-const generateHtmlPlugin = dir => {
+const parseHtmlPages = dir => {
   const files = fs.readdirSync(path.resolve(__dirname, dir));
 
   return files.reduce((acc, file) => {
@@ -24,135 +25,149 @@ const generateHtmlPlugin = dir => {
   }, []);
 }
 
-module.exports = {
-  entry: [
-    './src/index.js',
-    './src/styles.scss'
-  ],
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'js/bundle.min.js',
-  },
-  devServer: {
-    static: {
-      directory: path.resolve(__dirname, 'dist'),
-    },
-    port: 3000,
-    open: true,
-    hot: true,
-    compress: true,
-    historyApiFallback: {
-      rewrites: fs.readdirSync(path.resolve(__dirname, 'src/view')).map(file => {
-        const [name, extension] = file.split('.');
+const parseHtmlParts = dir => {
+  return fs.readdirSync(path.resolve(__dirname, dir)).map(file => {
+    const [name, extension] = file.split('.');
 
-        if (extension === 'html') {
-          return { from: new RegExp(`^\\/${name}`), to: `/${file}` };
-        } else if ( name === 'index' ) {
-          return { from: /./, to: `/${name}/index.html` };
-        }
-        return null;
-      }).filter(item => item !== null),
+    if (extension === 'html') {
+      return { from: new RegExp(`^\\/${name}`), to: `/${file}` };
+    } else if ( name === 'index' ) {
+      return { from: /./, to: `/${name}/index.html` };
+    }
+    return null;
+  }).filter(item => item !== null);
+}
+
+// separate and compile every scss file from root "src" folder
+const parseEntries = (type, outputFolder) => {
+  return glob.sync(`./src/**.${type}`).reduce((obj, el) => {
+    const name = path.parse(el).name;
+
+    obj[`${outputFolder}/${name}`]     = el;
+    obj[`${outputFolder}/${name}.min`] = el;
+    return obj;
+  }, {});
+}
+
+module.exports = (env, argv) => {
+  return {
+    entry: [
+      './src/index.js',
+      './src/styles.scss',
+    ],
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'js/bundle.min.js',
     },
-  },
-  performance : {
-    hints: false
-  },
-  mode: 'production',
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new CssMinimizerPlugin({
-        minimizerOptions: {
-          preset: [
-            'default',
+    plugins: [
+      new CleanWebpackPlugin({
+        protectWebpackAssets: false,
+        cleanAfterEveryBuildPatterns: ['*.LICENSE.txt'],
+      }),
+      new MiniCssExtractPlugin({
+        filename: 'css/style.min.css',
+      }),
+      new CopyPlugin({
+        patterns: [
+          {
+            from: 'src/fonts',
+            to: 'fonts',
+            noErrorOnMissing: true,
+          },
+          {
+            from: 'src/images',
+            to: 'images',
+            noErrorOnMissing: true,
+          },
+        ],
+      }),
+    ].concat(
+      parseHtmlPages('src/view')
+    ),
+    devServer: {
+      static: {
+        directory: path.resolve(__dirname, 'dist'),
+      },
+      port: 3000,
+      open: true,
+      hot: true,
+      compress: true,
+      historyApiFallback: {
+        rewrites: parseHtmlParts('src/view'),
+      },
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(sass|scss)$/,
+          include: path.resolve(__dirname, 'src/styles'),
+          use: [
             {
-              discardComments: { removeAll: true },
+              loader: MiniCssExtractPlugin.loader,
+              options: {},
+            },
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: false,
+                url: false,
+              },
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                postcssOptions: {
+                  plugins: [
+                    require('autoprefixer'),
+                  ],
+                },
+              },
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                implementation: require('sass'),
+                sourceMap: false,
+              },
             },
           ],
         },
-      }),
-      new TerserPlugin({
-        extractComments: false,
-        terserOptions: {
-          compress: false,
-          format: {
-            comments: false,
-            beautify: true,
-            quote_style: 1,
-            preamble: '/* X.js MIT license */',
-          },
-          keep_classnames: true, // save classes names
-          keep_fnames: true, // save functions names
-          mangle: false, // disable names obfuscation
-        },
-      }),
-    ],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(sass|scss)$/,
-        include: path.resolve(__dirname, 'src/styles'),
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {},
-          },
-          {
-            loader: 'css-loader',
-            options: {
-              sourceMap: false,
-              url: false,
-            },
-          },
-          {
-            loader: 'postcss-loader',
-            options: {
-              postcssOptions: {
-                plugins: [
-                  require('autoprefixer'),
-                ],
-              },
-            },
-          },
-          {
-            loader: 'sass-loader',
-            options: {
-              implementation: require('sass'),
-              sourceMap: false,
-            },
-          },
-        ],
-      },
-      {
-        test: /\.html$/,
-        include: path.resolve(__dirname, 'src/view/parts'),
-        use: ['raw-loader'],
-      },
-    ],
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: 'css/style.min.css',
-    }),
-    new CleanWebpackPlugin({
-      cleanAfterEveryBuildPatterns: ['*.LICENSE.txt'],
-    }),
-    new CopyPlugin({
-      patterns: [
         {
-          from: 'src/fonts',
-          to: 'fonts',
-          noErrorOnMissing: true,
-        },
-        {
-          from: 'src/images',
-          to: 'images',
-          noErrorOnMissing: true,
+          test: /\.html$/,
+          include: path.resolve(__dirname, 'src/view/parts'),
+          use: ['raw-loader'],
         },
       ],
-    }),
-  ].concat(
-    generateHtmlPlugin('src/view')
-  ),
+    },
+    optimization: {
+      minimize: true,
+      minimizer: [
+        new TerserPlugin({
+          extractComments: false,
+          terserOptions: {
+            compress: argv.mode === 'production',
+            format: {
+              comments: false,
+              beautify: true,
+              quote_style: 1,
+              preamble: '/* X.js MIT license */',
+            },
+            keep_classnames: true, // save classes names
+            keep_fnames: true, // save functions names
+            mangle: false, // disable names obfuscation
+          },
+        }),
+        new CssMinimizerPlugin({
+          minimizerOptions: {
+            preset: [
+              'default',
+              {
+                discardComments: { removeAll: true },
+              },
+            ]
+          },
+        }),
+      ],
+    },
+  };
 }
